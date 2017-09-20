3e194e038848062659a3b1c216e4d963fcc6dd69
Michael Merickel <michael@merickel.org>
2017-09-20 16:55:27 -0500

    lock transitions and cleanup when they are done

--- src/index.js
+++ src/index.js
@@ -77,6 +77,8 @@ export class ReactNativeModal extends Component {
     deviceHeight: Dimensions.get('window').height,
   };
 
+  transitionLock = null;
+
   constructor(props) {
     super(props);
     this._buildAnimations(props);
@@ -113,10 +115,11 @@ export class ReactNativeModal extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     // On modal open request, we slide the view up and fade in the backdrop
-    if (this.state.isVisible && !prevState.isVisible) {
+    if (this.props.isVisible && !prevProps.isVisible) {
       this._open();
-      // On modal close request, we slide the view down and fade out the backdrop
-    } else if (!this.props.isVisible && prevProps.isVisible) {
+    }
+    // On modal close request, we slide the view down and fade out the backdrop
+    else if (!this.props.isVisible && prevProps.isVisible) {
       this._close();
     }
   }
@@ -150,20 +153,36 @@ export class ReactNativeModal extends Component {
   };
 
   _open = () => {
+    if (this.transitionLock) return;
+    this.transitionLock = true;
     this.backdropRef.transitionTo(
       { opacity: this.props.backdropOpacity },
       this.props.backdropTransitionInTiming,
     );
     this.contentRef[this.animationIn](this.props.animationInTiming).then(() => {
-      this.props.onModalShow();
+      this.transitionLock = false;
+      if (!this.props.isVisible) {
+        this._close();
+      }
+      else {
+        this.props.onModalShow();
+      }
     });
   };
 
-  _close = async () => {
+  _close = () => {
+    if (this.transitionLock) return;
+    this.transitionLock = true;
     this.backdropRef.transitionTo({ opacity: 0 }, this.props.backdropTransitionOutTiming);
     this.contentRef[this.animationOut](this.props.animationOutTiming).then(() => {
-      this.setState({ isVisible: false });
-      this.props.onModalHide();
+      this.transitionLock = false;
+      if (this.props.isVisible) {
+        this._open();
+      }
+      else {
+        this.setState({ isVisible: false });
+        this.props.onModalHide();
+      }
     });
   };
 
