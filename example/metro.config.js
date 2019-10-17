180b46217f9d454bf3ab805ab2840ce1d5adb681
Anthony Cyrille <rewieer@gmail.com>
2019-10-17 10:38:42 +0200

    refactored development environment

--- example/metro.config.js
+++ example/metro.config.js
@@ -4,6 +4,10 @@
  *
  * @format
  */
+const path = require('path');
+const blacklist = require('metro-config/src/defaults/blacklist');
+
+const reactNativeModalRoot = path.resolve(__dirname, '..');
 
 module.exports = {
   transformer: {
@@ -13,5 +17,14 @@ module.exports = {
         inlineRequires: false,
       },
     }),
+  },
+  watchFolders: [reactNativeModalRoot],
+  resolver: {
+    extraNodeModules: {
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    },
+    blacklistRE: blacklist([
+      new RegExp(`${reactNativeModalRoot}/node_modules/react-native/.*`),
+    ]),
   },
 };
