919c16c99cb39710e52d4e05ebe381a3741a730a
Anthony Cyrille <rewieer@gmail.com>
2020-03-20 05:55:15 +0100

    refactor: example app

--- example/App.tsx
+++ example/App.tsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
 import Navigator from './src/Navigator';
 
 class App extends Component {
@@ -7,14 +6,5 @@ class App extends Component {
     return <Navigator />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-  },
-});
 
 export default App;
