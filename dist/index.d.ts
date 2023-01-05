2f985e7e5d07b09917e7342c9cdad45bd5e6388c
Eugene Shepel <eugene.shepel@eosda.com>
2023-01-05 14:04:01 +0200

    add dist folder

---
+++ dist/index.d.ts
@@ -0,0 +1,13 @@
+import {ReactNativeModal} from './modal';
+export {ModalProps, ReactNativeModal, OnSwipeCompleteParams} from './modal';
+export {
+  AnimationEvent,
+  Animations,
+  SupportedAnimation,
+  Orientation,
+  Direction,
+  PresentationStyle,
+  OnOrientationChange,
+  GestureResponderEvent,
+} from './types';
+export default ReactNativeModal;
