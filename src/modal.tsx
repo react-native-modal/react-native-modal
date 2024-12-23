7c1d23e0e2c16e5c69ba07bca80ae0f7989db62a
hje <hjerikson@gmail.com>
2024-12-23 11:55:43 +0100

    Fix: Prevent backdrop flicker when closing modal.
    Added explicit setting of backdrop opacity to ensure consistent opacity
    during the modal's lifecycle. Prevents backdrop flickering when closing
    the modal.

--- src/modal.tsx
+++ src/modal.tsx
@@ -232,7 +232,10 @@ export class ReactNativeModal extends React.Component<ModalProps, State> {
     }
   }
 
-  static getDerivedStateFromProps(nextProps: Readonly<ModalProps>, state: State) {
+  static getDerivedStateFromProps(
+    nextProps: Readonly<ModalProps>,
+    state: State,
+  ) {
     if (!state.isVisible && nextProps.isVisible) {
       return {isVisible: true, showContent: true};
     }
@@ -720,6 +723,7 @@ export class ReactNativeModal extends React.Component<ModalProps, State> {
           this.state.showContent && !hasCustomBackdrop
             ? backdropColor
             : 'transparent',
+        opacity: this.props.backdropOpacity,
       },
     ];
 
